748bb1dcc6b35185ae47eb18239ed8d942ca7d2e
Guilherme Sa <guilsa001@gmail.com>
2016-03-23 01:31:14 -0700

    Create Home component, set to IndexRoute

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -3,7 +3,10 @@ var React = require('react');
 var Nav = React.createClass({
   render: function () {
     return (
-      <h1>hello from Nav</h1>
+      <div>
+        <h1>hello from Nav</h1>
+        {this.props.children}
+      </div>
     )
   }
 })
--- app/config/routes.js
+++ app/config/routes.js
@@ -4,10 +4,12 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var Nav = require('../components/Nav');
+var Home = require('../components/Home');
 
 var routes = (
   <Router>
     <Route path="/" component={Nav}>
+      <IndexRoute component={Home} />
     </Route>
   </Router>
 );
