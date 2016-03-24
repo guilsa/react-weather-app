5a26f7aae5a6c52faf535d0e17777618fbcf7837
Guilherme Sa <guilsa001@gmail.com>
2016-03-23 19:35:35 -0700

    Install webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,14 @@ module.exports = {
       { test: /\.js$/,
         exclude: /node_modules/,
         loader: "babel-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false',
+            'url-loader?prefix=img/&limit=5000'
+        ]
       }
     ]
   },
