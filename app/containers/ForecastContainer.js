dded2e6e0f9564cc91eb3e070b34fa181934487a
Guilherme Sa <guilsa001@gmail.com>
2016-03-25 17:35:14 -0700

    ForecastContainer responsible for holding isLoading state

--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -1,10 +1,17 @@
 var React = require('react');
+var Results = require('../components/Results');
 
 var ForecastContainer = React.createClass({
+  getInitialState: function(){
+    return {
+      isLoading: true
+    }
+  },
   render: function () {
     return (
       <div>
-        <h1>forecast</h1>
+        <Results
+          isLoading={this.state.isLoading}/>
       </div>
     )
   }
