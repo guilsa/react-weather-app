4dd3f517d1c9b4af76592d1e1e4f200693f39d34
Guilherme Sa <guilsa001@gmail.com>
2016-03-27 10:45:46 -0700

    Implement Day container inside Forecast

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -1,22 +1,9 @@
-// On our example Github Battle ex app MainContainer is loaded here
-// I think on that case it just served as a jumbotron component
-// where multiple routes and components rendered inside of it
+var React = require('react');
+var PropTypes = React.PropTypes;
+var Loading = require('../components/Loading');
+var Day = require('./Day');
 
 var styles = {
-  // container: {
-  //   display: 'flex',
-  //   alignItems: 'center',
-  // },
-  container: {
-    paddingLeft: 100,
-    paddingRight: 100,
-    display:'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
   header: {
     paddingTop: 20,
     fontSize: 55,
@@ -24,37 +11,21 @@ var styles = {
   },
   title: {
     paddingTop: 20,
+    paddingBottom: 5,
     fontSize: 30,
     fontWeight: 100,
   },
-  box: {
-    width: 207,
-    height: 193,
-    minWidth: 207,
-    backgroundColor: "green",
-    margin: 30,
+  dayContainer: {
     display:'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignContent: 'flex-start',
     alignItems: 'flex-start',
-  },
-  body: {
-    paddingTop: 10,
-    fontSize: 29,
-    fontWeight: 100,
-  },
-  image: {
-    width: 128,
-    height: 130,
-    backgroundColor: 'red'
+    paddingLeft: 50,
+    paddingRight: 50,
   }
 }
-
-var React = require('react');
-var PropTypes = React.PropTypes;
-var Loading = require('../components/Loading');
 
 function Forecast (props) {
  if (props.isLoading === true) {
@@ -64,29 +35,16 @@ function Forecast (props) {
  }
   return (
     <div>
-      <h1 className="text-center" style={styles.header}>{props.city}</h1>
+      <h1 className="text-center" style={styles.header}>{props.forecast.data.city.name}</h1>
       <h1 className="text-center" style={styles.title}>Select a day</h1>
-      <div style={styles.container}>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
+      <div style={styles.dayContainer}>
+        {
+          props.forecast.data.list.map(function(item, index){
+            return (
+              <Day key={index} weather={item} />
+            );
+          })
+        }
       </div>
     </div>
   )
@@ -94,7 +52,7 @@ function Forecast (props) {
 
 Forecast.propTypes = {
   isLoading: PropTypes.bool.isRequired,
-  city: PropTypes.string.isRequired
+  forecast: PropTypes.object.isRequired
 }
 
 module.exports = Forecast;
