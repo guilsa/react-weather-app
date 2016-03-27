a6a14cb8a35fb8dc28bb9c2b565c71674d63bb11
Guilherme Sa <guilsa001@gmail.com>
2016-03-26 18:41:10 -0700

    Add styling to Forecast container

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,6 +2,56 @@
 // I think on that case it just served as a jumbotron component
 // where multiple routes and components rendered inside of it
 
+var styles = {
+  // container: {
+  //   display: 'flex',
+  //   alignItems: 'center',
+  // },
+  container: {
+    paddingLeft: 100,
+    paddingRight: 100,
+    display:'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  header: {
+    paddingTop: 20,
+    fontSize: 55,
+    fontWeight: 100,
+  },
+  title: {
+    paddingTop: 20,
+    fontSize: 30,
+    fontWeight: 100,
+  },
+  box: {
+    width: 207,
+    height: 193,
+    minWidth: 207,
+    backgroundColor: "green",
+    margin: 30,
+    display:'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  body: {
+    paddingTop: 10,
+    fontSize: 29,
+    fontWeight: 100,
+  },
+  image: {
+    width: 128,
+    height: 130,
+    backgroundColor: 'red'
+  }
+}
+
 var React = require('react');
 var PropTypes = React.PropTypes;
 var Loading = require('../components/Loading');
@@ -14,7 +64,30 @@ function Forecast (props) {
  }
   return (
     <div>
-      <h1>{props.city}</h1>
+      <h1 className="text-center" style={styles.header}>{props.city}</h1>
+      <h1 className="text-center" style={styles.title}>Select a day</h1>
+      <div style={styles.container}>
+        <div style={styles.box}>
+          <div style={styles.image}></div>
+          <div style={styles.body}>Saturday, Mar 26</div>
+        </div>
+        <div style={styles.box}>
+          <div style={styles.image}></div>
+          <div style={styles.body}>Saturday, Mar 26</div>
+        </div>
+        <div style={styles.box}>
+          <div style={styles.image}></div>
+          <div style={styles.body}>Saturday, Mar 26</div>
+        </div>
+        <div style={styles.box}>
+          <div style={styles.image}></div>
+          <div style={styles.body}>Saturday, Mar 26</div>
+        </div>
+        <div style={styles.box}>
+          <div style={styles.image}></div>
+          <div style={styles.body}>Saturday, Mar 26</div>
+        </div>
+      </div>
     </div>
   )
 }
