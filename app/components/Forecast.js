33f10ed526f01acf70e6276216a3ddeef7f77563
Guilherme Sa <guilsa001@gmail.com>
2016-03-27 10:45:46 -0700

    Implement Day container inside Forecast

--- app/components/Forecast.js
+++ app/components/Forecast.js
@@ -2,21 +2,12 @@
 // I think on that case it just served as a jumbotron component
 // where multiple routes and components rendered inside of it
 
+var React = require('react');
+var PropTypes = React.PropTypes;
+var Loading = require('../components/Loading');
+var Day = require('./Day');
+
 var styles = {
-  // container: {
-  //   display: 'flex',
-  //   alignItems: 'center',
-  // },
-  container: {
-    paddingLeft: 100,
-    paddingRight: 100,
-    display:'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
   header: {
     paddingTop: 20,
     fontSize: 55,
@@ -27,34 +18,17 @@ var styles = {
     fontSize: 30,
     fontWeight: 100,
   },
-  box: {
-    width: 207,
-    height: 193,
-    minWidth: 207,
-    backgroundColor: "green",
-    margin: 30,
+  Day: {
     display:'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
     alignContent: 'flex-start',
     alignItems: 'flex-start',
-  },
-  body: {
-    paddingTop: 10,
-    fontSize: 29,
-    fontWeight: 100,
-  },
-  image: {
-    width: 128,
-    height: 130,
-    backgroundColor: 'red'
   }
 }
 
-var React = require('react');
-var PropTypes = React.PropTypes;
-var Loading = require('../components/Loading');
+CITY = {"data":{"city":{"id":1273840,"name":"Connaught Place","coord":{"lon":77.216667,"lat":28.633333},"country":"IN","population":0},"cod":"200","message":0.0068,"cnt":5,"list":[{"dt":1458972000,"temp":{"day":297.15,"min":297.15,"max":297.15,"night":297.15,"eve":297.15,"morn":297.15},"pressure":999.36,"humidity":54,"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"speed":4.21,"deg":300,"clouds":44},{"dt":1459058400,"temp":{"day":303.21,"min":293.04,"max":305.79,"night":293.57,"eve":304.12,"morn":293.04},"pressure":1000.38,"humidity":58,"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"speed":2.74,"deg":304,"clouds":48},{"dt":1459144800,"temp":{"day":301.02,"min":288.32,"max":303.4,"night":292.84,"eve":302.11,"morn":288.32},"pressure":1000.87,"humidity":60,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":0.86,"deg":265,"clouds":0},{"dt":1459231200,"temp":{"day":300.66,"min":288.11,"max":303.49,"night":293.45,"eve":302.58,"morn":288.11},"pressure":998.85,"humidity":55,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":2.83,"deg":312,"clouds":0},{"dt":1459317600,"temp":{"day":301.96,"min":289.27,"max":304.93,"night":293.75,"eve":304.09,"morn":289.27},"pressure":999.12,"humidity":52,"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"speed":2.12,"deg":276,"clouds":56}]},"status":200,"statusText":"OK","headers":{"content-type":"application/json; charset=utf-8"},"config":{"transformRequest":{},"transformResponse":{},"headers":{"Accept":"application/json, text/plain, */*"},"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","method":"get","url":"http://api.openweathermap.org/data/2.5/forecast/daily?q=undefined&type=accurate&APPID=c58172ee6b3cb9558c6ac7060b0bf36c&cnt=5"}}
 
 function Forecast (props) {
  if (props.isLoading === true) {
@@ -66,27 +40,14 @@ function Forecast (props) {
     <div>
       <h1 className="text-center" style={styles.header}>{props.city}</h1>
       <h1 className="text-center" style={styles.title}>Select a day</h1>
-      <div style={styles.container}>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
-        <div style={styles.box}>
-          <div style={styles.image}></div>
-          <div style={styles.body}>Saturday, Mar 26</div>
-        </div>
+      <div style={styles.Day}>
+        {
+          CITY.data.list.map(function(item, index){
+            return (
+              <Day key={index} weather={item.weather[0].main} />
+            );
+          })
+        }
       </div>
     </div>
   )
