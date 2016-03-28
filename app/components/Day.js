f58514a4647460cfa30080e016c866b2c9079f85
Guilherme Sa <guilsa001@gmail.com>
2016-03-28 13:58:52 -0700

    Update Day with props, parent data

--- app/components/Day.js
+++ app/components/Day.js
@@ -3,21 +3,8 @@ var PropTypes = React.PropTypes;
 var utils = require('../utils/utils');
 
 styles = {
-  container: {
-    paddingLeft: 100,
-    paddingRight: 100,
-    display:'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignContent: 'flex-start',
-    alignItems: 'flex-start',
-  },
   box: {
-    width: 207,
-    height: 193,
     minWidth: 207,
-    backgroundColor: "green",
     margin: 30,
     display:'flex',
     flexDirection: 'column',
@@ -34,25 +21,19 @@ styles = {
   image: {
     width: 128,
     height: 130,
-    backgroundColor: 'red'
   }
 }
 
-// function getImage (props) {
-//   return {
-//
-//   }
-// }
-
 function Day (props) {
+  var icon = props.weather.weather[0].icon;
+  var date = utils.getDate(props.weather.dt);
+
   return (
     <div>
-      <div style={styles.container}>
-        <div style={styles.box}>
-          <img style={styles.image} src="app/images/weather-icons/01d.svg"/>
-          <div style={styles.body}>
-            {props.weather}
-          </div>
+      <div style={styles.box}>
+        <img style={styles.image} src={ "app/images/weather-icons/" + icon + ".svg" }/>
+        <div style={styles.body}>
+          {date}
         </div>
       </div>
     </div>
@@ -60,7 +41,7 @@ function Day (props) {
 }
 
 Day.propTypes = {
-  weather: PropTypes.string.isRequired,
+  weather: PropTypes.object.isRequired,
 }
 
 module.exports = Day;
