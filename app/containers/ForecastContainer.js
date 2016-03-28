ed74d4660459c59377b2b3eaaa7b81704be59200
Guilherme Sa <guilsa001@gmail.com>
2016-03-28 14:00:04 -0700

    Update ForecastContainer stub

--- app/containers/ForecastContainer.js
+++ app/containers/ForecastContainer.js
@@ -2,7 +2,9 @@ var React = require('react');
 var Forecast = require('../components/Forecast');
 var openWeatherHelpers = require('../utils/openWeatherHelpers');
 
-DATA = {"data":{"city":{"id":1273840,"name":"Connaught Place","coord":{"lon":77.216667,"lat":28.633333},"country":"IN","population":0},"cod":"200","message":0.0068,"cnt":5,"list":[{"dt":1458972000,"temp":{"day":297.15,"min":297.15,"max":297.15,"night":297.15,"eve":297.15,"morn":297.15},"pressure":999.36,"humidity":54,"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03n"}],"speed":4.21,"deg":300,"clouds":44},{"dt":1459058400,"temp":{"day":303.21,"min":293.04,"max":305.79,"night":293.57,"eve":304.12,"morn":293.04},"pressure":1000.38,"humidity":58,"weather":[{"id":802,"main":"Clouds","description":"scattered clouds","icon":"03d"}],"speed":2.74,"deg":304,"clouds":48},{"dt":1459144800,"temp":{"day":301.02,"min":288.32,"max":303.4,"night":292.84,"eve":302.11,"morn":288.32},"pressure":1000.87,"humidity":60,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":0.86,"deg":265,"clouds":0},{"dt":1459231200,"temp":{"day":300.66,"min":288.11,"max":303.49,"night":293.45,"eve":302.58,"morn":288.11},"pressure":998.85,"humidity":55,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":2.83,"deg":312,"clouds":0},{"dt":1459317600,"temp":{"day":301.96,"min":289.27,"max":304.93,"night":293.75,"eve":304.09,"morn":289.27},"pressure":999.12,"humidity":52,"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04d"}],"speed":2.12,"deg":276,"clouds":56}]},"status":200,"statusText":"OK","headers":{"content-type":"application/json; charset=utf-8"},"config":{"transformRequest":{},"transformResponse":{},"headers":{"Accept":"application/json, text/plain, */*"},"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","method":"get","url":"http://api.openweathermap.org/data/2.5/forecast/daily?q=undefined&type=accurate&APPID=c58172ee6b3cb9558c6ac7060b0bf36c&cnt=5"}}
+// stub
+MARTINEZ = {"data":{"city":{"id":5370868,"name":"Martinez","coord":{"lon":-122.134132,"lat":38.019371},"country":"US","population":0},"cod":"200","message":0.0088,"cnt":5,"list":[{"dt":1459195200,"temp":{"day":288.15,"min":281.81,"max":288.33,"night":281.81,"eve":285.34,"morn":286.1},"pressure":1019.07,"humidity":51,"weather":[{"id":801,"main":"Clouds","description":"few clouds","icon":"02d"}],"speed":9.31,"deg":339,"clouds":24},{"dt":1459281600,"temp":{"day":290.17,"min":278.76,"max":290.67,"night":278.76,"eve":284.81,"morn":281.1},"pressure":1018.13,"humidity":50,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":5.66,"deg":337,"clouds":0},{"dt":1459368000,"temp":{"day":293.87,"min":277.99,"max":294.64,"night":281.63,"eve":288.19,"morn":277.99},"pressure":1019.45,"humidity":61,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":3.37,"deg":339,"clouds":0},{"dt":1459454400,"temp":{"day":293.59,"min":280.19,"max":294.62,"night":280.35,"eve":288.86,"morn":280.19},"pressure":1026.39,"humidity":72,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":1.83,"deg":259,"clouds":0},{"dt":1459540800,"temp":{"day":289.31,"min":278.1,"max":292.84,"night":282.53,"eve":292.84,"morn":278.1},"pressure":1017.96,"humidity":0,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":1.49,"deg":236,"clouds":0}]},"status":200,"statusText":"OK","headers":{"content-type":"application/json; charset=utf-8"},"config":{"transformRequest":{},"transformResponse":{},"headers":{"Accept":"application/json, text/plain, */*"},"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","method":"get","url":"http://api.openweathermap.org/data/2.5/forecast/daily?q=Martinez, CA&type=accurate&APPID=c58172ee6b3cb9558c6ac7060b0bf36c&cnt=5"}}
+WINSTON = {"data":{"city":{"id":4499612,"name":"Winston-Salem","coord":{"lon":-80.244217,"lat":36.099861},"country":"US","population":0},"cod":"200","message":0.0086,"cnt":5,"list":[{"dt":1459184400,"temp":{"day":294.6,"min":282.55,"max":294.6,"night":282.55,"eve":289.23,"morn":294.6},"pressure":998.72,"humidity":63,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":7.11,"deg":294,"clouds":0},{"dt":1459270800,"temp":{"day":290.31,"min":279.28,"max":291.64,"night":279.28,"eve":286.92,"morn":280.3},"pressure":1006.85,"humidity":63,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":3.41,"deg":340,"clouds":0},{"dt":1459357200,"temp":{"day":290.17,"min":277.08,"max":291.5,"night":285.73,"eve":288.24,"morn":277.08},"pressure":1013.08,"humidity":56,"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"speed":4.46,"deg":151,"clouds":0},{"dt":1459443600,"temp":{"day":293.67,"min":286.86,"max":293.67,"night":289.44,"eve":290.12,"morn":286.86},"pressure":999.61,"humidity":73,"weather":[{"id":502,"main":"Rain","description":"heavy intensity rain","icon":"10d"}],"speed":7.72,"deg":183,"clouds":80,"rain":19.31},{"dt":1459530000,"temp":{"day":295.26,"min":288.53,"max":295.26,"night":288.53,"eve":292.44,"morn":291.65},"pressure":989.59,"humidity":0,"weather":[{"id":502,"main":"Rain","description":"heavy intensity rain","icon":"10d"}],"speed":7.72,"deg":240,"clouds":96,"rain":13.95}]},"status":200,"statusText":"OK","headers":{"content-type":"application/json; charset=utf-8"},"config":{"transformRequest":{},"transformResponse":{},"headers":{"Accept":"application/json, text/plain, */*"},"timeout":0,"xsrfCookieName":"XSRF-TOKEN","xsrfHeaderName":"X-XSRF-TOKEN","method":"get","url":"http://api.openweathermap.org/data/2.5/forecast/daily?q=Winston-Salem, NC&type=accurate&APPID=c58172ee6b3cb9558c6ac7060b0bf36c&cnt=5"}}
 
 // ForecastContainer holds state for city and isLoading
 // It also loads Forecast.
@@ -12,22 +14,22 @@ var ForecastContainer = React.createClass({
   getInitialState: function(){
     return {
       isLoading: true,
-      city: ""
+      forecast: ""
     }
   },
   componentDidMount: function(){
-    // openWeatherHelpers.getFiveDayForecast(this.state.location)
+    // Not sure if for this.props.params.city I should be using another comp's props
+    // Or if it's ok to get URL param
+    // openWeatherHelpers.getFiveDayForecast(this.props.params.city)
     //   .then(function(forecast){
     //     console.log(JSON.stringify(forecast));
     //     this.setState({
-    //       city: forecast,
+    //       forecast: forecast,
     //       isLoading: false
     //     })
     //   }.bind(this))
-    console.log(DATA.data.list); // forecast
-    console.log(DATA.data.city.name); // city name
     this.setState({
-      city: DATA.data.city.name,
+      forecast: WINSTON,
       isLoading: false
     })
   },
@@ -36,7 +38,7 @@ var ForecastContainer = React.createClass({
       <div>
         <Forecast
           isLoading={this.state.isLoading}
-          city={this.state.city} />
+          forecast={this.state.forecast} />
       </div>
     )
   }
