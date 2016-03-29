bfd7058202417cfca05651d07d9f8554f43b4918
Guilherme Sa <guilsa001@gmail.com>
2016-03-28 22:29:36 -0700

    Fix CityDetails data flow

--- app/components/CityDetails.js
+++ app/components/CityDetails.js
@@ -1,11 +1,20 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var Day = require('./Day');
+
+var styles = {
+  header: {
+    paddingTop: 10,
+    fontSize: 29,
+    fontWeight: 100,
+  }
+}
 
 function CityDetails (props) {
-  console.log(props.weather);
+  debugger
   return (
     <div>
-      DETAILS
+      <Day weather={props.weather}></Day>
     </div>
   )
 }
--- app/containers/CityDetailsContainer.js
+++ app/containers/CityDetailsContainer.js
@@ -1,22 +1,12 @@
 var React = require('react');
 var CityDetails = require('../components/CityDetails');
-var openWeatherHelpers = require('../utils/openWeatherHelpers');
 
 var CityDetailsContainer = React.createClass({
-  getInitialState: function(){
-    return {
-      weather: ""
-    }
-  },
-  componentDidMount: function(){
-    this.setState({
-      weather: this.props.location.state.weather
-    })
-  },
   render: function () {
     return (
-      <CityDetails
-        weather={this.state.weather}/>
+      <div>
+        <CityDetails weather={this.props.location.state.weather} />
+      </div>
     )
   }
 });
